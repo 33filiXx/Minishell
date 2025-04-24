09be85406f87df260db7b4327de772aaa7e4cfbb
Walid El Mjiyad <wel-mjiy@e2r9p10.1337.ma>
2025-04-24 17:18:10 +0100

    more lexer

--- Minishell_42/pars/parsing.c
+++ Minishell_42/pars/parsing.c
@@ -6,12 +6,23 @@
 /*   By: wel-mjiy <wel-mjiy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/18 20:12:47 by wel-mjiy          #+#    #+#             */
-/*   Updated: 2025/04/23 10:52:16 by wel-mjiy         ###   ########.fr       */
+/*   Updated: 2025/04/24 17:17:19 by wel-mjiy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../nrc/minishell.h"
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
 void	ft_free(char **ptr)
 {
 	int	i;
@@ -26,7 +37,7 @@ void	ft_free(char **ptr)
 	ptr = NULL;
 }
 
-void	store_into_nodes(char **str , t_lexer *lexer)
+void	store_into_nodes(char **str, t_lexer *lexer)
 {
 	int		i;
 	int		j;
@@ -41,26 +52,26 @@ void	store_into_nodes(char **str , t_lexer *lexer)
 		j = 0;
 		while (new_str[j] != NULL)
 		{
-			if (new_str[j][p] == '|')
-				insert_at_end(&lexer , new_str[j] , PIPE);
-			if (new_str[j][p] == '>')
-				insert_at_end(&lexer , new_str[j] , TRUNC);
-			if (new_str[j][p] == '<')
-				insert_at_end(&lexer , new_str[j] , INPUT);
-			if (new_str[j][p] == '>' && new_str[j][p + 1] == '>')
-				insert_at_end(&lexer , new_str[j] , APPEND);
-			if (new_str[j][p] == '<' && new_str[j][p + 1] == '<')
-				insert_at_end(&lexer , new_str[j] , HERDOC);
+			if (ft_strcmp(new_str[j], "|") == 0)
+				insert_at_end(&lexer, new_str[j], PIPE);
+			else if (ft_strcmp(new_str[j], ">") == 0)
+				insert_at_end(&lexer, new_str[j], TRUNC);
+			else if (ft_strcmp(new_str[j], "<") == 0)
+				insert_at_end(&lexer, new_str[j], INPUT);
+			else if (ft_strcmp(new_str[j], ">>") == 0)
+				insert_at_end(&lexer, new_str[j], APPEND);
+			else if (ft_strcmp(new_str[j], "<<") == 0)
+				insert_at_end(&lexer, new_str[j], HERDOC);
 			else
-				insert_at_end(&lexer , new_str[j] , WORD);
-		//	free(*new_str);
+				insert_at_end(&lexer, new_str[j], WORD);
+			//	free(*new_str);
 			j++;
 		}
 		i++;
 	}
 }
 
-void	parsing(char **argv , t_lexer *lexer)
+void	parsing(char **argv, t_lexer *lexer)
 {
-	store_into_nodes(argv , lexer);
+	store_into_nodes(argv, lexer);
 }
