cc0cb62aa448f534e4b3fe9aed7bb440542e23e0
Yasser Khoussi <ykhoussi@e1r5p5.1337.ma>
2025-05-12 20:55:44 +0100

    export_handel

--- Minishell_42/exec/builtin/export.c
+++ Minishell_42/exec/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: ykhoussi <ykhoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 20:27:30 by ykhoussi          #+#    #+#             */
-/*   Updated: 2025/05/11 22:33:41 by ykhoussi         ###   ########.fr       */
+/*   Updated: 2025/05/12 20:55:31 by ykhoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,28 @@
 
 // Update an existing environment variable.
 
-t_env	*sort_env(t_env *env)
-{
+// t_env	*sort_env(t_env *env)
+// {
 
+// }
+
+int	is_valid_identifier(char *s)
+{
+	int	i;
+
+	i = 1;
+	if (!s || (!ft_isalpha(s[0]) && s[0] != '_'))
+		return (1);
+	if (s[i])
+	{	while (s[i])
+		{
+			if (!ft_isalnum(s[i]))
+				return (1);
+			i++;
+		}
+	}
+	else
+		return (0);
 }
 
 void	print_env_export(t_env *env)
@@ -42,27 +61,13 @@ int	export_builtin(t_env **env, char **args)
 {
 	int	i;
 	char	*equal;
-	char	*key;
-	char	*value;
 
 	i = 1;
 	if (!args[1])
 		print_env_export(*env);
 	while (args[i])
 	{
-		equal = ft_strchr(args[i], '=');
-		if (equal)
-		{
-			key = ft_substr(args[i], 0, equal - args[i]);
-			value = ft_strdup(equal + 1);
-			set_env_value(env, key, value);
-			free(key);
-			free(value);
-		}
-		else
-		{
-			
-		}
+		
 	}
 	return (0);
 }
